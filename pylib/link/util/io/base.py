bada310a63ddcafe87d218ba989600f225b5f7bc
eddiehoyle <eddiepie@gmail.com>
2014-09-02 22:54:27 +1200

    Started refactoring file handlers

--- pylib/link/util/io/base.py
+++ pylib/link/util/io/base.py
@@ -12,7 +12,7 @@ from link.config import Config
 
 log = logging.getLogger(__name__)
 
-class FileHandler(object):
+class FileHandler2(object):
     def __init__(self, key):
 
         self.config = Config()
@@ -28,7 +28,6 @@ class FileHandler(object):
     def write(self, data):
         """Write data as json to disk"""
 
-        data = data or self.get_data()
         log.debug("Writing data key(s): %s" % len(data.keys()))
 
         self._create_dir()
@@ -49,3 +48,77 @@ class FileHandler(object):
             log.debug("Reading data key(s): %s" % len(data.keys()))
 
         return data
+
+#!/usr/bin/env python
+
+"""
+Settings node file handling
+"""
+
+import os
+import json
+import logging
+
+from link.util.io.base import FileHandler
+from link.config import Config
+
+log = logging.getLogger(__name__)
+
+class FileHandler(FileHandler):
+
+    def __init__(self, relative_path):
+        self.dir = os.path.join(self.config.root, 'resources')
+        self.path = os.path.join(self.dir, rel_path)
+        self.data = {}
+
+    def init_data(self):
+        """Override method"""
+        pass
+
+    def get_data(self):
+        return self.data
+
+    def set_data(self, data):
+        self.data = data
+
+    def write(self):
+        """Write data as json to disk"""
+
+        log.debug("Writing data to disk: %s" % self.path)
+
+        if not self.path:
+            raise IOError("No path set.")
+
+        if not os.path.exists(self.dir):
+            os.mkdir(self.dir)
+
+        with open(self.path, 'w') as f:
+            json.dump(self.data, f)
+
+        return self.path
+
+    def read(self):
+        """Read from json file on disk"""
+
+        log.debug("Reading data from disk: %s" % self.path)
+
+        if not os.path.exists(self.path):
+            raise IOError("Data file does not exist: %s" % self.path)
+
+        data = {}
+        with open(self.path, 'r') as f:
+            try:
+                self.data = json.loads(f.read())
+            except Exception as e:
+                log.error("Failed to parse json data file: %s" % self.path)
+                log.error(e)
+
+        self.set_data(data)
+
+        return self.data
+
+    def eval(self):
+        """Override method"""
+        pass
+
+    
--- pylib/link/util/io/settings.py
+++ pylib/link/util/io/settings.py
@@ -7,16 +7,61 @@ Settings node file handling
 import os
 import json
 import logging
+from maya import cmds
 
 from link.util.io.base import FileHandler
 from link.config import Config
 
 log = logging.getLogger(__name__)
 
+"""
+Example use:
+
+h = SettingsFileHandler('data/settings/L_foot_0.json')
+h.init_data("L_foot_0_settingsShape")
+h.write()
+
+h.read()
+
+h.eval()
+"""
+
 class SettingsFileHandler(FileHandler):
 
-    def __init__(self, key):
-        super(SettingsFileHandler, self).__init__(key)
+    def __init__(self, relative_path):
+        super(SettingsFileHandler, self).__init__(relative_path)
 
-        self.dir = self.config.get_settings_dir()
-        self.path = os.path.join(self.dir, "%s.json" % self.key)
+    def init_data(self, node):
+        """
+        Scan node for user defined attrs. Store settings node
+        and all attrs and values.
+        """
+
+        attrs = cmds.listAttr(node, ud=True)
+
+        data = {}
+        data['node'] = node
+        for attr in attrs:
+            data['attrs'] = [attr, cmds.getAttr("%s.%s" % (node, attr))]
+
+        self.set_data(data)
+
+    def eval(self):
+        """
+        Try restore all settings back onto node if exist
+        and not connected.
+        """
+
+        data = self.read()
+
+        node = data['node']
+        attrs = data['attrs']
+
+        for attr_value in attrs:
+            attr, value = attr_value
+            full_path = "%s.%s" % (node, attr)
+
+            if cmds.objExists(full_path):
+                if not cmds.listConnections(full_path, source=True, d=False) or []:
+                    cmds.setAttr(full_path, value)
+
